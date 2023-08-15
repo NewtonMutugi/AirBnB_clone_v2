0d64fa71c057de8b94204eaaec2919c97ae0b866
NewtonMutugi <newtonmut197@gmail.com>
2023-08-16 01:32:05 +0300

    Added web_flask/3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,7 +28,7 @@ def display_text(text):
 @app.route('/python/<text>')
 def display_python(text):
     """ Python"""
-    return "Python" + text.replace('_', ' ')
+    return "Python " + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
