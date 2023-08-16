2f81db3f0b77b5839e7b55156250794258c060f4
NewtonMutugi <newtonmut197@gmail.com>
2023-08-16 18:03:45 +0300

    Modifed models/*

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from sqlalchemy import Column, String
+import models
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
@@ -8,6 +9,12 @@ from models.place import place_amenity
 
 class Amenity(BaseModel):
     """Amenity class"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if models.storage_type == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
